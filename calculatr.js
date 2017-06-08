44d19538b55c482954617ed0430f1ee37a44caa6
Yu Koga <ukoooga@gmail.com>
2017-06-08 15:53:07 +0900

    0 when start&push c

--- calculatr.js
+++ calculatr.js
@@ -20,7 +20,7 @@ var point = document.getElementById("point");
 var equals = document.getElementById("equals");
 
 clrbtn.onclick = function clearall(){
-    numbox.value = null;
+    numbox.value = "0";
   }
 
 plumi.onclick = function plumi(){
@@ -46,42 +46,72 @@ for (x=0; x<numbers.length; x++){
 */
 
 seven.onclick = function enterSeven(){
+if (numbox.value === "0"){
+  numbox.value = "";
+}
   numbox.value += 7;
 }
 
 eight.onclick = function enterEight(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 8;
 }
 
 nine.onclick = function enterNine(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 9;
 }
 
 four.onclick = function enterFour() {
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 4;
 }
 
 five.onclick = function enterFive(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 5;
 }
 
 six.onclick = function enterSix(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 6;
 }
 
 one.onclick = function enterOne(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 1;
 }
 
 two.onclick = function enterTwo(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 2;
 }
 
 three.onclick = function enterThree(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 3;
 }
 
 zero.onclick = function enterZero(){
+  if (numbox.value === "0"){
+    numbox.value = "";
+  }
   numbox.value += 0;
 }
 
