be712a3ef2e74c290802837478e66cae8141fe2a
Yu Koga <ukoooga@gmail.com>
2017-06-19 18:32:40 +0900

    untangled

--- calculatr.js
+++ calculatr.js
@@ -22,9 +22,6 @@ var operator = 0;
 var dot = 0;
 
 function fontResize (){
-  if (numbox.value === "0"){
-    numbox.value = "";
-  }
   if (numbox.value.length > 6) {
     numbox.style.fontSize = "500%";
   }
@@ -34,7 +31,6 @@ function fontResize (){
   if (numbox.value.length > 12) {
     numbox.style.fontSize = "300%";
   }
-  operator = 0;
 }
 
 clrbtn.onclick = function clearall(){
@@ -65,99 +61,6 @@ percent.onclick = function percentify(){
   }
 }
 
-/*
-var numbers = [one, two, three, four, five, six, seven, eight, nine, zero];
-switch (numbers) {
-  case [0]:
-
-}
-
-switch (true) {
-  case numbox.value.length > 7:
-    numbox.style.fontSize = "600%";
-    break;
-  case numbox.value.length > 8:
-    numbox.style.fontSize = "500%";
-    break;
-  case numbox.value.length > 9:
-    numbox.style.fontSize = "400%";
-    break;
-  case numbox.value.length >10:
-    numbox.style.fontSize = "250%";
-    break;
-}
-*/
-
-seven.onclick = function enterSeven(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 7;
-  }
-}
-
-eight.onclick = function enterEight(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 8;
-  }
-}
-
-nine.onclick = function enterNine(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 9;
-  }
-}
-
-four.onclick = function enterFour() {
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 4;
-  }
-}
-
-five.onclick = function enterFive(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 5;
-  }
-}
-
-six.onclick = function enterSix(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 6;
-  }
-}
-
-one.onclick = function enterOne(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 1;
-  }
-}
-
-two.onclick = function enterTwo(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 2;
-  }
-}
-
-three.onclick = function enterThree(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 3;
-  }
-}
-
-zero.onclick = function enterZero(){
-  fontResize();
-  if (numbox.value.length < 18) {
-    numbox.value += 0;
-  }
-}
-
 point.onclick = function enterPoint(){
   fontResize();
   if (numbox.value.length < 18 && dot === 0) {
@@ -165,6 +68,30 @@ point.onclick = function enterPoint(){
     dot = 1;
   }
 }
+
+function numButton(numWord, num) {
+  fontResize();
+  numWord.onclick = function enterNum() {
+    if (numbox.value === "0"){
+      numbox.value = "";
+    }
+    if (numbox.value.length < 15){
+      numbox.value += num;
+      operator = 0;
+    }
+  }
+}
+
+numButton(seven, 7);
+numButton(eight, 8);
+numButton(nine, 9);
+numButton(four, 4);
+numButton(five, 5);
+numButton(six, 6);
+numButton(one, 1);
+numButton(two, 2);
+numButton(three, 3);
+numButton(zero, 0);
 
 /*const equation = numbox.value;*/
 
