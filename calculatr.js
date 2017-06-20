481ce23fa9aaf5e29a04297d0036ebf4bb26939c
Yu Koga <ukoooga@gmail.com>
2017-06-20 16:59:08 +0900

    fontsize auto adjust, input new value after ans

--- calculatr.js
+++ calculatr.js
@@ -18,26 +18,29 @@ var plusbtn = document.getElementById("plusbtn");
 var zero = document.getElementById("zero");
 var point = document.getElementById("point");
 var equals = document.getElementById("equals");
+var equalbtn = 0;
 var operator = 0;
 var dot = 0;
 var values = [];
 
 function fontResize (){
-  if (numbox.value.length > 6) {
+if (numbox.value.length > 6 && numbox.value.length <= 9) {
     numbox.style.fontSize = "500%";
-  }
-  if (numbox.value.length > 9) {
+  } else if (numbox.value.length > 9 && numbox.value.length <= 12) {
     numbox.style.fontSize = "400%";
-  }
-  if (numbox.value.length > 12) {
-    numbox.style.fontSize = "300%";
+  } else if (numbox.value.length > 12) {
+    numbox.style.fontSize = "290%";
+  } else {
+    numbox.style.fontSize = "700%";
   }
 }
 
 clrbtn.onclick = function clearall(){
     numbox.value = "0";
     operator = 0;
+    equalbtn = 0;
     dot = 0;
+    values = [];
     numbox.style.fontSize = "700%";
   }
 
@@ -71,19 +74,20 @@ point.onclick = function enterPoint(){
 }
 
 function numButton(numWord, num) {
-  fontResize();
   numWord.onclick = function enterNum() {
-    if (numbox.value === "0"){
+    if (numbox.value === "0" || equalbtn === 1){
       numbox.value = "";
+      equalbtn = 0;
     }
     if (operator !== 0){
       values[0] = numbox.value;
       numbox.value = "";
     }
-    if (numbox.value.length < 15){
+    if (numbox.value.length < 18){
       numbox.value += num;
       operator = 0;
     }
+    fontResize();
   }
 }
 
@@ -100,10 +104,20 @@ numButton(zero, 0);
 
 function operation (opebtn, opeValue, opeSign) {
   opebtn.onclick = function operate(){
+  /*  if (equalbtn === 0 && operator !== 0) {
+      values[2] = numbox.value;
+      var tempans = eval(values[0]+values[1]+values[2]);
+      numbox.value = tempans;
+      fontResize();
+      values[1] = opeSign;
+      operator = opeValue;
+      dot = 0;
+    }*/
     if (operator !== opeValue) {
       values[1] = opeSign;
       operator = opeValue;
       dot = 0;
+      equalbtn = 0;
     }
   }
 }
@@ -114,8 +128,10 @@ operation (plusbtn, 3, "+");
 operation (minusbtn, 4, "-");
 
 equals.onclick = function calculate(){
-var answer = eval(values[0]+values[1]+numbox.value);
+values[2] = numbox.value;
+var answer = eval(values[0]+values[1]+values[2]);
 numbox.value = answer;
 fontResize();
 operator = 0;
+equalbtn = 1;
 }
